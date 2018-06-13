ec23a6e93b719b0b165b2e2258985f178fbeba34
AZ92 <armundas.zemaitaitis@gmail.com>
2018-06-13 10:04:03 +0300

    perdarymas antros eiles postu

--- category.php
+++ category.php
@@ -2,54 +2,53 @@
 
 <div class="container">
    <div class="row add-top-margin">
+
      <?php
 
-          $i=0;
+     $i=0;
 
-          while (have_posts()) {
+     if ( have_posts() ) :
+       while ( have_posts() ) : the_post();
 
-            $i++;
+      $i++;
 
-            if ($i==1) {
+          if ($i==1) {
+        ?>
 
-            ?>
-
-              <div class="col-lg-12 first-post">
-                <div class="col-lg-6 post-thumb">
-                  <a href="<?php the_permalink(); ?>">
-                  <?php the_post_thumbnail('medium_large', ['class' => 'img-responsive', 'title' => 'Feature image']); ?>
-                  </a>
-                </div>
-                <div class="col-lg-6 post-text">
-                  <a href="<?php the_permalink(); ?>">
-                  <h1><?php    the_title();?></h1>
-                  <h3> <?php the_excerpt();?></h3>
-                  <?php the_category();?>
-                 </a>
-                </div>
-              </div>
-
-            <?php
-            }
-
-            if ($i != 1 && $i < 3) {
-
-            ?>
-
-               <div class="col-lg-4 antri-post">
-               <?php the_post_thumbnail('post-thumbnail', ['class' => 'img-responsive', 'title' => 'Feature image']); ?>
-               <?php	the_title();?>
+        <div class="col-lg-12 first-post">
+             <div class="col-lg-6 post-thumb">
+               <a href="<?php the_permalink(); ?>">
+               <?php the_post_thumbnail('medium_large', ['class' => 'img-responsive', 'title' => 'Feature image']); ?>
+               </a>
+             </div>
+             <div class="col-lg-6 post-text">
+               <a href="<?php the_permalink(); ?>">
+               <h1><?php    the_title();?></h1>
+               <h3> <?php the_excerpt();?></h3>
                <?php the_category();?>
-               </div>
+              </a>
+             </div>
+        </div>
 
+        <?php
+          }
+
+          elseif ($i != 1 && $i <= 3) {
+            ?>
+            
+            <div class="col-lg-4 antri-post">
+               <?php the_post_thumbnail('post-thumbnail', ['class' => 'img-responsive', 'title' => 'Feature image']); ?>
+               <h3><?php	the_title();?></h3>
+               <h5><?php the_category();?></h5>
+            </div>
+            <hr>
             <?php
-            }
+          }
 
-            if ($i != 2 && $i >= 3) {
+          elseif ($i != 1 && $i > 3) {
+            ?>
 
-              ?>
-
-              <div class="col-lg-offset-1 col-lg-10 third-line">
+            <div class="col-lg-offset-2 col-lg-10 third-line">
               <div class="col-lg-4">
                 <?php the_post_thumbnail('post-thumbnail', ['class' => 'img-responsive', 'title' => 'Feature image']); ?>
               </div>
@@ -61,9 +60,15 @@
             </div>
 
             <?php
-            }
           }
-          ?>
+
+         ?>
+
+     <?php endwhile; else : ?>
+
+          <p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+
+    <?php endif; ?>
 
   </div>
 </div>
